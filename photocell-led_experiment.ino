363396cc27fb362c58a64fa792f8d0d2ba4c4f10
Utkarsh Chauhan <56200899+utkarshchauhan021@users.noreply.github.com>
2020-09-23 17:56:02 +0530

    added program to blink an led using photocell

---
+++ photocell-led_experiment.ino
@@ -0,0 +1,23 @@
+int sensorPin = A0;
+int led = 7;
+int lightCal;
+int lightVal;
+
+void setup() {
+  //pinMode(led, OUTPUT);
+  lightCal = analogRead(sensorPin);
+}
+
+void loop() {
+  lightVal = analogRead(sensorPin);
+  
+  if (lightVal < lightCal-20 ) {
+    //digitalWrite(led, HIGH);
+    tone(9,10);
+  }
+  
+  else {
+    //digitalWrite (led, LOW);
+    noTone(9);
+  }
+}
