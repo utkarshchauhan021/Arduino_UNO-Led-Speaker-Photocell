3f1ec35f255362c9d030c74efbfd6f3ecd0ddfa2
Utkarsh Chauhan <56200899+utkarshchauhan021@users.noreply.github.com>
2020-10-23 21:10:43 +0530

    tonemelody

--- tonemelody.ino
+++ tonemelody.ino
@@ -1,6 +1,3 @@
-/*************************************************
- * Public Constants
- *************************************************/
 #define NOTE_B0  31
 #define NOTE_C1  33
 #define NOTE_CS1 35
@@ -91,12 +88,10 @@
 #define NOTE_D8  4699
 #define NOTE_DS8 4978
 
-// notes in the melody:
 int melody[] = {
   NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
 };
 
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
   4, 8, 8, 4, 4, 4, 4, 4
 };
@@ -106,16 +101,12 @@ void setup() {
 }
 
 void loop() {
-  // iterate over the notes of the melody:
   for (int thisNote = 0; thisNote < 8; thisNote++) {
 
-    // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / noteDurations[thisNote];
     tone(8, melody[thisNote], noteDuration);
 
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
