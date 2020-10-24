381788638b10cb39a1646519075ab96664fa36ea
Utkarsh Chauhan <56200899+utkarshchauhan021@users.noreply.github.com>
2020-10-24 20:19:20 +0530

    multi_fade

--- multi-fade.ino
+++ multi-fade.ino
@@ -1,7 +1,6 @@
 int led1 = 9;
 int led2 = 6;  
 
-// the setup routine runs once when you press reset:
 void setup() {
   pinMode(led1,OUTPUT);
   pinMode(led2,OUTPUT);
@@ -31,22 +30,4 @@ void loop() {
       brightness--;
   }
   delay(100);
-//    for (int brightness=0; brightness<100; brightness++) {
-//      analogWrite (led1, brightness);
-//      delay(5);
-//    }
-//    for (int brightness=99; brightness>=0; brightness--) {
-//      analogWrite (led1, brightness);
-//      delay(5);
-//    }
-//    delay(100);
-//    for (int brightness=0; brightness<100; brightness++){
-//      analogWrite (led2, brightness);
-//      delay (5);
-//    }
-//    for (int brightness=99; brightness>=0; brightness--) {
-//      analogWrite (led2, brightness);
-//      delay (5);
-//    }
-//    delay(100);
 }
